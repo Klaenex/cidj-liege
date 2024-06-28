251797638431790e60879c93ce2f396dc8e6f844
Klaenex <hello@cuozzovincenzo.be>
2024-06-28 15:13:55 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Animations from "./pages/Animations";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <div className="App">test</div>;
+  const location = useLocation();
+  return (
+    <>
+      <Routes location={location} key={location.pathname}>
+        <Route exact path="/" component={Home} />
+        <Route path="/animations/" component={Animations} />
+        <Route path="/services/" component={Services} />
+        <Route path="/contact/" component={Contact} />
+
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Animations.js
@@ -0,0 +1,12 @@
+// src/pages/Home.js
+import React from "react";
+
+function Animations() {
+  return (
+    <div>
+      <h1>Animations Page</h1>
+    </div>
+  );
+}
+
+export default Animations;
